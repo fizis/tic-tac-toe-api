4b0a5fec70b5820d940b083d9862e9d7fd2825ea
mesdievai <vytautas.ciuksys@gmail.com>
2019-03-22 15:50:34 +0200

    maintain game board inside the API

--- models/game.js
+++ models/game.js
@@ -6,29 +6,40 @@ const firstMoveMarker = 'x';
 const Game = function(id) {
     this.id = id;
     this.moves = [];
+    this.board = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ];
 }
 
 Game.prototype.makeMove = function(x, y) {
     // TODO: validate if it's possible to make such a move
     // e.g. valid coords and not a duplicate move
-    let move = new Move(x, y);
+    
+    let marker = firstMoveMarker;
 
     // TODO: extract to private method
-    if (this.moves.length == 0) {
-        move.marker = firstMoveMarker;
-    }
-    else {
+    if (this.moves.length > 0) {
         let lastMove = this.moves[this.moves.length - 1];
         let lastMoveMarker = lastMove.marker;
 
         if (lastMoveMarker === 'x') {
-            move.marker = 'o';
+            marker = 'o';
         } else {
-            move.marker = 'x';
+            marker = 'x';
         }
     }
 
+    let move = new Move(x, y, marker);
+
     this.moves.push(move);
+    this.updateBoard(move);
+}
+
+// TODO: make this private
+Game.prototype.updateBoard = function(move) {
+    this.board[move.y][move.x] = move.marker;
 }
 
 module.exports = Game;
