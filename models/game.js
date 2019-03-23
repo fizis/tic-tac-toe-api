4cdcd747b42427bfefec869547c787b427071e65
mesdievai <vytautas.ciuksys@gmail.com>
2019-03-24 00:49:08 +0200

    use ES6 classes for models

--- models/game.js
+++ models/game.js
@@ -2,135 +2,129 @@ const Move = require('./move');
 
 const firstMoveMarker = 'x';
 
-// TODO: use class syntax instead
-const Game = function(id) {
-    this.id = id;
-    this.ended = false;
-    this.winner = '';
-    this.board = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
-    ];
-    this.moves = [];
-}
+class Game {
+    constructor(id) {
+        this.id = id;
+        this.ended = false;
+        this.winner = '';
+        this.board = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+        ];
+        this.moves = [];
+    }
 
-Game.prototype.makeMove = function(x, y) {
-    if (x < 0 || x > 2) return;
-    if (y < 0 || y > 2) return;
-    if (this.board[x][y] !== '') return;
+    makeMove(x, y) {
+        if (x < 0 || x > 2) return;
+        if (y < 0 || y > 2) return;
+        if (this.board[x][y] !== '') return;
+        
+        let marker = this._getMoveMarker();
+        let move = new Move(x, y, marker);
     
-    let marker = this.getMoveMarker();
-    let move = new Move(x, y, marker);
-
-    this.moves.push(move);
-
-    this.updateBoard(move);
-    this.checkWinner();
-    this.checkEnded();
-}
-
-// TODO: make this private
-Game.prototype.getMoveMarker = function() {
-    let marker = firstMoveMarker;
-
-    if (this.moves.length > 0) {
-        let lastMove = this.moves[this.moves.length - 1];
-        let lastMoveMarker = lastMove.marker;
-
-        if (lastMoveMarker === 'x') {
-            marker = 'o';
-        } else {
-            marker = 'x';
-        }
-    }
-
-    return marker;
-}
-
-// TODO: make this private
-Game.prototype.updateBoard = function(move) {
-    this.board[move.x][move.y] = move.marker;
-}
-
-// TODO: make this private
-Game.prototype.checkWinner = function() {
-    console.log(this.board);
-
-    let winner = this.getWinnerByCol(this.board);
-
-    if (winner !== '') {
-        this.winner = winner;
-        return;
-    }
-
-    var transposedBoard = this.board[0].map((col, i) => this.board.map(row => row[i]));
-
-    console.log(transposedBoard);
+        this.moves.push(move);
     
-    winner = this.getWinnerByCol(transposedBoard);
-
-    if (winner !== '') {
-        this.winner = winner;
-        return;
-    }
-
-    this.winner = this.getWinnerByDiag(this.board);
-}
-
-// TODO: make this private
-Game.prototype.getWinnerByCol = function(board) {
-    for(let i = 0; i < board.length; i++) {
-        // console.log(board[i])
-        let col = board[i].join('');
-
-        if (col === 'ooo') {
-            return 'o';
-        }
-
-        if (col === 'xxx') {
-            return 'x';
-        }
+        this._updateBoard(move);
+        this._checkWinner();
+        this._checkEnded();
     }
     
-    return '';
-}
+    _getMoveMarker() {
+        let marker = firstMoveMarker;
 
-// TODO: make this private
-Game.prototype.getWinnerByDiag = function(board) {
-    let diag1 = '';
-    let diag2 = '';
+        if (this.moves.length > 0) {
+            let lastMove = this.moves[this.moves.length - 1];
+            let lastMoveMarker = lastMove.marker;
 
-    for(let i = 0; i < board.length; i++) {
-        diag1 += board[i][i];
-        diag2 += board[i][board.length - i - 1];
-    }
-    
-    if (diag1 === 'ooo' || diag2 === 'ooo') {
-        return 'o';
-    }
-
-    if (diag1 === 'xxx' || diag2 === 'xxx') {
-        return 'x';
-    }
-
-    return '';
-}
-
-// TODO: make this private
-Game.prototype.checkEnded = function() {
-    this.ended = this.winner !== ''
-    
-    if (!this.ended) {
-        for(let i = 0; i < this.board.length; i++) {
-            for(let j = 0; j < this.board[i].length; j++) {
-                if (this.board[i][j] === '') {
-                    return;
-                }
+            if (lastMoveMarker === 'x') {
+                marker = 'o';
+            } else {
+                marker = 'x';
             }
         }
 
-        this.ended = true;
+        return marker;
+    }
+
+    _updateBoard(move) {
+        this.board[move.x][move.y] = move.marker;
+    }
+
+    _checkWinner() {
+        console.log(this.board);
+
+        let winner = this._getWinnerByCol(this.board);
+
+        if (winner !== '') {
+            this.winner = winner;
+            return;
+        }
+
+        var transposedBoard = this.board[0].map((col, i) => this.board.map(row => row[i]));
+
+        console.log(transposedBoard);
+        
+        winner = this._getWinnerByCol(transposedBoard);
+
+        if (winner !== '') {
+            this.winner = winner;
+            return;
+        }
+
+        this.winner = this._getWinnerByDiag(this.board);
+    }
+
+    _getWinnerByCol(board) {
+        for(let i = 0; i < board.length; i++) {
+            let col = board[i].join('');
+
+            if (col === 'ooo') {
+                return 'o';
+            }
+
+            if (col === 'xxx') {
+                return 'x';
+            }
+        }
+        
+        return '';
+    }
+
+    _getWinnerByDiag(board) {
+        let diag1 = '';
+        let diag2 = '';
+
+        for(let i = 0; i < board.length; i++) {
+            diag1 += board[i][i];
+            diag2 += board[i][board.length - i - 1];
+        }
+        
+        if (diag1 === 'ooo' || diag2 === 'ooo') {
+            return 'o';
+        }
+
+        if (diag1 === 'xxx' || diag2 === 'xxx') {
+            return 'x';
+        }
+
+        return '';
+    }
+
+    _checkEnded() {
+        this.ended = this.winner !== ''
+        
+        if (!this.ended) {
+            for(let i = 0; i < this.board.length; i++) {
+                for(let j = 0; j < this.board[i].length; j++) {
+                    if (this.board[i][j] === '') {
+                        return;
+                    }
+                }
+            }
+
+            this.ended = true;
+        }
     }
 }
 
--- models/move.js
+++ models/move.js
@@ -1,7 +1,9 @@
-const Move = function(x, y, marker) {
-    this.x = x;
-    this.y = y;
-    this.marker = marker;
+class Move {
+    constructor(x, y, marker) {
+        this.x = x;
+        this.y = y;
+        this.marker = marker;
+    }
 }
 
 module.exports = Move;
