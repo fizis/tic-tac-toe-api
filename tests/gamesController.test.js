ca0ecd21598d86634d263ad4052747310265cd2c
mesdievai <vytautas.ciuksys@gmail.com>
2019-03-24 16:33:39 +0200

    finish two UT

--- tests/gamesController.test.js
+++ tests/gamesController.test.js
@@ -4,7 +4,6 @@ const GameRepository = require('../repositories/gameRepository');
 const GamesController = require('../controllers/gamesController');
 
 const gameRepository = new GameRepository();
-const gamesController = new GamesController(gameRepository);
 
 const mockResponse = () => {
   const res = {};
@@ -16,21 +15,37 @@ const mockResponse = () => {
 };
 
 it('creates game', () => {
+  const game = new Game('test_id');
+  
+  gameRepository.createGame = jest.fn(() => {
+    return game;
+  });
+  
+  const gamesController = new GamesController(gameRepository);
   const req = httpMocks.createRequest({});
   const res = mockResponse();
 
   gamesController.createGame(req, res);
-    
+  
+  expect(gameRepository.createGame).toHaveBeenCalledTimes(1);
   expect(res.status).toHaveBeenCalledWith(200);
-  expect(res.send).toHaveBeenCalledWith(expect.any(Game)); // TODO: check for particular Game
+  expect(res.send).toHaveBeenCalledWith(game);
 });
 
 it('gets all games', () => {
+  const games = [new Game('test_id')];
+  
+  gameRepository.getAllGames = jest.fn(() => {
+    return games;
+  });
+
   const req = httpMocks.createRequest({});
   const res = mockResponse();
+  const gamesController = new GamesController(gameRepository);
 
   gamesController.getAllGames(req, res);
-    
+  
+  expect(gameRepository.getAllGames).toHaveBeenCalledTimes(1);
   expect(res.status).toHaveBeenCalledWith(200);
-  //expect(res.send).toHaveBeenCalledWith(expect.any([])); // TODO: check for particular array (two cases: both empty and not)
+  expect(res.send).toHaveBeenCalledWith(games);
 });
