10b9bca125ddbd32709a5d2a81310fec84f1fcb6
mesdievai <vytautas.ciuksys@gmail.com>
2019-03-24 13:03:10 +0200

    couple of unit tests

---
+++ tests/gamesController.test.js
@@ -0,0 +1,32 @@
+const httpMocks = require('node-mocks-http');
+const gamesController = require('../controllers/gamesController');
+const Game = require('../models/game');
+
+const mockResponse = () => {
+  const res = {};
+
+  res.status = jest.fn().mockReturnValue(res);
+  res.send = jest.fn().mockReturnValue(res);
+
+  return res;
+};
+
+it('creates game', () => {
+  const req = httpMocks.createRequest({});
+  const res = mockResponse();
+
+  gamesController.createGame(req, res);
+    
+  expect(res.status).toHaveBeenCalledWith(200);
+  expect(res.send).toHaveBeenCalledWith(expect.any(Game)); // TODO: check for particular Game
+});
+
+it('gets all games', () => {
+  const req = httpMocks.createRequest({});
+  const res = mockResponse();
+
+  gamesController.getAllGames(req, res);
+    
+  expect(res.status).toHaveBeenCalledWith(200);
+  //expect(res.send).toHaveBeenCalledWith(expect.any([])); // TODO: check for particular array (two cases: both empty and not)
+});
